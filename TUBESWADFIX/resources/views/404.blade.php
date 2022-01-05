d1bce65d33d84a6c3eb02c94e2eb10c9daf047c3
Fatih <Fadhillahfatih@gmail.com>
2022-01-05 16:09:25 +0700

    update

--- TUBESWADFIX/resources/views/404.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.app')
-@section('content')
-    <div class="container">
-        <div class="col-sm-12 mt-4 text-center">
-            <h3>Access Forbidden</h3>
-        </div>
-    </div>
-@stop
